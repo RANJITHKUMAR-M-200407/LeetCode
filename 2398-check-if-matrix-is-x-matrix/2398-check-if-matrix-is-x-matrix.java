7a163823a16cb1c14e61121d16944f0ee577c01c
RANJITHKUMAR-M-200407 <ranjithk14532@gmail.com>
2025-03-01 19:20:14 +0530

    Time: 3 ms (25.84%), Space: 45.2 MB (84.97%) - LeetHub

---
+++ 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+       for(int i=0;i<grid.length;i++)
+       {
+        for(int j=0;j<grid[i].length;j++)
+        {
+            if(i==j || j==grid[i].length-i-1 ) 
+            {
+                if(grid[i][j]==0)
+                {
+                   return false;
+                }
+            }
+            else if (grid[i][j]!=0) return false; 
+        }
+       } 
+       return true;
+    }
+}
