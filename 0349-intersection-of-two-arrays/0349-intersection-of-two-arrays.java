e7fd5335f10d0b0b64f03efb37b4e105fd251a15
RANJITHKUMAR-M-200407 <ranjithk14532@gmail.com>
2025-03-28 11:35:21 +0530

    Time: 7 ms (9.93%), Space: 43 MB (94.34%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list3 = new ArrayList<>();
-        for(int i=0;i<nums1.length;i++)
-        {
-            if(!list1.contains(nums1[i])) list1.add(nums1[i]);
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    if(!list.contains(nums2[j])){
+                        list.add(nums2[j]);
+                    }
+                }
+            }
         }
-           for(int i=0;i<nums2.length;i++)
-        {
-            if(!list3.contains(nums2[i])) list3.add(nums2[i]);
+        int arr[] = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i] = list.get(i);
         }
-
-          ArrayList<Integer> list2 = new ArrayList<>();
-        for(int i=0;i<list3.size();i++)
-        {
-            if(list1.contains(list3.get(i))) list2.add(list3.get(i));
-        }
-        int[]ans = new int[list2.size()];
-        for(int i=0;i<list2.size();i++)
-        {
-         ans[i]=list2.get(i);
-        }
-       return ans;
+        return arr;
     }
 }
