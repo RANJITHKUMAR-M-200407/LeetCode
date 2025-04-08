1f10e101ac992a2703e38dae10b52dfc28e0f40d
RANJITHKUMAR-M-200407 <ranjithk14532@gmail.com>
2025-04-08 22:22:55 +0530

    Time: 2 ms (85.07%), Space: 45.2 MB (29.27%) - LeetHub

---
+++ 1704-special-positions-in-a-binary-matrix/1704-special-positions-in-a-binary-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int count1=0;
+        int ans=0;
+        int res=0;
+        int c1v=0;
+        for(int i=0;i<mat.length;i++)
+        {
+            count1=0;
+            for(int j=0;j<mat[i].length;j++)
+            {
+               if(mat[i][j]==1)
+               {
+                c1v=j;
+                count1++;
+               } 
+            }
+            if(count1==1)
+            {
+             if(arr(mat,c1v)==1)
+             {
+                res++;
+             }
+            }
+        }
+        return res;
+    }
+    public static int arr(int[][]mat,int num)
+    {
+        int num1=0;
+        for(int i=0;i<mat.length;i++)
+        {
+            if(mat[i][num]==1)  num1++;
+        }
+        return num1;
+    }
+}
