d416f8ef9186ca419e919abf2d93aecb959b3cca
RANJITHKUMAR-M-200407 <ranjithk14532@gmail.com>
2025-07-13 08:46:34 +0530

    Time: 26 ms (66.18%), Space: 58.1 MB (41.36%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i=0,j=0;
+        int count=0;
+        while(i<players.length && j<trainers.length){
+           while(j<trainers.length && players[i]>trainers[j]){
+            j++;
+           }
+           while((i<players.length && j<trainers.length) && players[i]<=trainers[j]){
+            count++;
+            i++;
+            j++;
+           }
+        }
+        return count;
+    }
+}
