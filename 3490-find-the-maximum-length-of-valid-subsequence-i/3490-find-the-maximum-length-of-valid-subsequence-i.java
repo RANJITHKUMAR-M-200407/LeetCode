5e93e34e751ba5b28eb62b4ea4043d9b40788291
RANJITHKUMAR-M-200407 <ranjithk14532@gmail.com>
2025-07-16 08:55:56 +0530

    Time: 9 ms (31.97%), Space: 59.7 MB (100%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,20 +1,17 @@
 class Solution {
     public int maximumLength(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
         list.add(nums[0]);
+        int count1=0;
+        int count2=0;
         for(int i=1;i<nums.length;i++){
+                  if(nums[i]%2==0)  count1++;
+            if(nums[i]%2!=0)  count2++;
             if(nums[i]%2==0 && list.get(list.size()-1)%2!=0)  list.add(nums[i]);
             else if(nums[i]%2!=0 && list.get(list.size()-1)%2==0) list.add(nums[i]);
         }
-         for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0)  list1.add(nums[i]);
-            else  list2.add(nums[i]);
-        }
-        return Math.max(list.size(),Math.max(list1.size(),list2.size()));
-
-       // return list.size();
-        
+      if(nums[0]%2==0) count1++;
+      else count2++;
+        return Math.max(list.size(),Math.max(count1,count2));
     }
 }
