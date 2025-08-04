2eae0f511711e6725ff198d88815449985667ca2
RANJITHKUMAR-M-200407 <ranjithk14532@gmail.com>
2025-08-04 08:17:05 +0530

    Time: 34 ms (28.19%), Space: 54.4 MB (68.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,9 +5,9 @@
  */
 var twoSum = function(nums, target) {
     const ans =[];
-    for(let i=0;i<nums.length;i++){
-        for(let j=0;j<nums.length;j++){
-            if(i!=j && nums[i]+nums[j]==target)
+    for(let i=0;i<nums.length-1;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target)
             {
                 ans[0]=i;
                 ans[1]=j;
